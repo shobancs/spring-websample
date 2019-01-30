992c5bd8c522a5d38a7ee417e4efe5236f4fc1d5
Shoban Cheekuru <scheekur@cisco.com>
2019-01-29 20:48:17 -0500

    Sample spring boot source code

---
+++ initial/src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/api/hello")
+    public String index() {
+        return "Hello Greetings from Spring Boot !";
+    }
+    
+}
